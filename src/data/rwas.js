c4d3c2c727f36c98e2e5c376ec07792a658ef015
Luke Judges <lukejudges@MAC-1C1867.station>
2025-08-04 23:30:00 +0200

    addVERT

--- src/data/rwas.js
+++ src/data/rwas.js
@@ -1,7 +1,17 @@
 const rwaData = [
   {
     "region": "Americas",
-    "assets": []
+    "assets": [
+      {
+        "name": "VERT Agricultural Receivables",
+        "issuer": "rsU8y8bB6E7Q2oGi2ZZxqNauUq85fk5R7K",
+        "currency": "VRT",
+        "amount": 139125000,
+        "city": "São Paulo",
+        "lat": -23.5505,
+        "lng": -46.6333
+      }
+    ]  
   },
   {
     "region": "Europe, Middle East & Africa",
